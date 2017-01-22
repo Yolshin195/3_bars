976a4253657bcae7b53e0d333ae92f7f184b38d7
Yolshin195 <elshin195@gmail.com>
2017-01-23 01:32:26 +0500

    23.01.17

--- bars.py
+++ bars.py
@@ -2,7 +2,7 @@
 
 import json
 import sys
-import os.path 
+import os.path
 import math
 
 
@@ -22,21 +22,29 @@ def get_smallest_bar(data):
 
 
 def get_closest_bar(data, longitude, latitude):
-    return min(data, key=lambda min_key: distBar(min_key['Latitude_WGS84'], latitude, min_key['Longitude_WGS84'], longitude))
-     
+    return min(data, key=lambda min_key: distBar(
+        min_key['Latitude_WGS84'], latitude,
+        min_key['Longitude_WGS84'], longitude
+    ))
 
-def distBar( latitude1, latitude2, Longitude1, longitude2, rad=6372795):
+
+def convertInRadians(coordinate):
+    return float(coordinate) * math.pi/180.
+
+
+def distBar(latitude1, latitude2, Longitude1, longitude2, rad=6372795):
     """
+        сверическая теорема косинусов
         pi число pi rad - радиус сферы(Земли)
-        argument: lat1=number, lat2=number, long1=number, long2=number 
+        dist - растояние между двумя точками
     """
-    #кординаты двух точек в радианах
-    lat1 = float(latitude1) * math.pi/180.
-    lat2 = float(latitude2) * math.pi/180.
-    long1 = float(Longitude1) * math.pi/180.
-    long2 = float(longitude2) * math.pi/180.
-    
-    #косинусы и синусы широт и разницы долгот
+    # кординаты двух точек в радианах
+    lat1 = convertInRadians(latitude1)
+    lat2 = convertInRadians(latitude2)
+    long1 = convertInRadians(Longitude1)
+    long2 = convertInRadians(longitude2)
+
+    # косинусы и синусы широт и разницы долгот
     cl1 = math.cos(lat1)
     cl2 = math.cos(lat2)
     sl1 = math.sin(lat1)
@@ -45,28 +53,53 @@ def distBar( latitude1, latitude2, Longitude1, longitude2, rad=6372795):
     cdelta = math.cos(delta)
     sdelta = math.sin(delta)
 
-    #вычисление длинны большого круга
-    y = math.sqrt(math.pow(cl2*sdelta, 2)) + math.pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2)
+    # вычисление длинны большого круга
+    y = math.sqrt(math.pow(cl2*sdelta, 2)) + math.pow(
+            cl1 * sl2 - sl1 * cl2 * cdelta, 2
+        )
     x = sl1 * sl2 + cl1 * cl2 * cdelta
-    ad = math.atan2(y,x)
+    ad = math.atan2(y, x)
     dist = ad * rad
 
     return dist
 
-def output(data, text):
-    return("""*** {} ***\n Название: {Name} 
-    Адресс: {Address}
-    """.format(text, **data))
-
-
-def main(v, data):
-    test ={
-            "1": [get_biggest_bar(data), "Самый большой бар"],
-            "2": [get_smallest_bar(data), "Самый маленький бар"],
-            "3": [get_closest_bar(data, input("Ведите долготу: "), input("Введите широту: ")), "Ближе всего квам расположен бар"]
-            }
-    return output(test[v][0], test[v][1])
 
 if __name__ == '__main__':
     data = load_json_data(sys.argv[1])
-    print(main(input(), data))
+    dataOutpyt = None
+
+    print("""
+    1 - Самый большой бар
+    2 - самый маленький бар
+    3 - Ближайший бар по отношения к вам
+    """)
+
+    i = input("Ведите число от 1 до 3: ")
+
+    if i == "1":
+        dataOutpyt = get_biggest_bar(data), "Самый большой бар"
+
+    elif i == "2":
+        dataOutpyt = get_smallest_bar(data), "Самый маленький бар"
+
+    elif i == "3":
+        dataOutpyt = get_closest_bar(
+            data,
+            input("Введите широту: "),
+            input("Введите долготу: ")
+        ), "Самый ближайший бар"
+
+    else:
+        print("Ошибка ввода")
+
+    if dataOutpyt:
+        print("""
+    *** {} ***
+
+    Название: {Name}
+    Административный округ: {AdmArea}
+    Район: {District}
+    Адрес: {Address}
+    Контактные данные: {PublicPhone[0][PublicPhone]}
+    Количество сидячих мест: {SeatsCount}
+        """.format(dataOutpyt[1], **dataOutpyt[0]))
